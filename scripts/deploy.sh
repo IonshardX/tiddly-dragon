424ef7ced4367432bf85a2a1fad4a5e6e765231d
Corey Ling <corey.ling@cenx.com>
2017-01-09 22:11:39 -0500

    Add deploy script for new TiddlyWiki website

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf output
+scripts/build.sh
+tiddlywiki --build index
+git checkout tiddlers/\$__StoryList.tid
+git checkout gh-pages
+cp output/index.html .
+git add .
+git commit
+git push origin gh-pages
+git checkout master
